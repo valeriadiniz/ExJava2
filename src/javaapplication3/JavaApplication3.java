7152e8592658d78dd2ef2127cca0ae803af9d5e9
Valéria Diniz <valeria.diniz@fatec.sp.gov.br>
2020-09-15 01:52:14 -0300

    commit exJFrame

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *pacote referente a tarefa 2 entrega 14/09/2020
+ * 
+ * 
+                               Criar os exercícios utilizando JFrame
+
+1. Fazer um algoritmo que leia um número inteiro e escreva o seu antecessor e o seu sucessor.
+
+2. Ler uma medida em polegadas e imprimir a equivalente em centímetros, sabendo que 2.54 cm equivale a 1 polegada.
+
+3. Faça um algoritmo que receba o salário-base de um funcionário, calcule e mostre o salário a receber, sabendo-se que esse funcionário
+tem gratificação de 5% sobre o salário-base e paga imposto de 7% sobre o total.
+
+4. Faça um algoritmo que receba o ano de nascimento de uma pessoa e o ano atual, calcule e mostre:
+a) a idade atual dessa pessoa. b) quantos anos ela terá em 2038.
+
+5. Faça um algoritmo para ler dois números inteiros e informar se estes são iguais ou diferentes.
+
+6. Uma loja fornece 10% de desconto para funcionários e 5% de desconto para clientes vips. Faça um programa que calcule o valor total a
+ser pago por uma pessoa. O programa deverá ler o valor total da compra efetuada e um código que identifique se o comprador é um cliente
+comum (1), funcionário (2) ou vip (3).
+
+7. Escrever um programa para ler um número inteiro e informar se ele é divisível por 5.
+
+8. Faça um programa que o usuário informe o salário recebido e o total gasto. Deverá ser exibido na tela “Gastos dentro do orçamento” 
+caso o valor gasto não ultrapasse o valor do salário e “Orçamento estourado” se o valor gasto ultrapassar o valor do salário.
+
+9. Elabore um algoritmo que dada a idade de um nadador classifica-o em uma das seguintes categorias:
+
+infantil A = 5 - 7 anos
+
+infantil B = 8-10 anos
+
+juvenil A = 11-13 anos
+
+juvenil B = 14-17 anos
+
+adulto = maiores de 18 anos
+
+10. Entrar com um número inteiro positivo e exibir o fatorial deste número, lembrando que 0! = 1. Exemplo: 5! = 5x4x3x2x1 = 120
+
+
+ * @author valer
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
